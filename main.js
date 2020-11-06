291993bca540e66716349977783b9b16e787edaa
Susana <su.gonzalez.vejo@gmail.com>
2020-11-06 17:46:25 +0100

    Convert anonymous functions into arrow functions

--- main.js
+++ main.js
@@ -1,74 +1,52 @@
-'use strict';
+"use strict";
 
+//Global variables
+const submit = document.querySelector(".js-submit");
+let userInput = document.querySelector(".js-input");
+const tip = document.querySelector(".js-tip");
+let trycounter = document.querySelector(".js-counter");
 
-//constantes a usar
-
-const submit = document.querySelector('.js-submit');
-let userInput = document.querySelector('.js-input');
-const tip = document.querySelector('.js-tip');
-let trycounter = document.querySelector('.js-counter');
-
-//funcion para generar un número aleatorio
-
+//Random Number function
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
+  return Math.ceil(Math.random() * max);
 }
 
 const randomNumber = getRandomNumber(100);
-console.log('Your random numer is', randomNumber);
+console.log("Your random numer is", randomNumber);
 
-
-//función para adivinar el númeor y sus supuestos
-
+//Guess number function and conditionals
 function guessNumber() {
-    let userNumber = parseInt(userInput.value);
-    let message = '';
+  let userNumber = parseInt(userInput.value);
+  let text = "";
 
-    // let userNumber = document.querySelector('.js-input').value;
-    //const randomNumber = getRandomNumber(100);
+  if (userNumber > randomNumber && userNumber <= 100) {
+    text = "demasiado alto";
+  } else if (userNumber < randomNumber) {
+    text = "demasiado bajo";
+  } else if (userNumber === randomNumber) {
+    text = "has ganado campeona";
+  } else if (userNumber > 100 || userNumber === 0) {
+    text = "el número debe estar entre 1 y 100";
+  }
 
-
-    if (userNumber > randomNumber && userNumber <= 100) {
-        message = 'demasiado alto';
-
-    } else if (userNumber < randomNumber) {
-        message = 'demasiado bajo';
-
-
-    } else if (userNumber === randomNumber) {
-        message = 'has ganado campeona';
-
-
-    } else if (userNumber > 100 || userNumber === 0) {
-        message = 'el número debe estar entre 1 y 100';
-    }
-
-    //contar los intentos
-
-    counter();
-    tip.innerHTML = (`Tu número es ${message}`);
-
+  counter();
+  showFeedback(text);
 }
 
-// Move the feedback logic to a function and execute it on each click.
+//
 
+//Feedback function
+const showFeedback = (text) => {
+  const tip = document.querySelector(".js-tip");
+  tip.innerHTML = text;
+};
 
-
-// Move you counter logic into a function and execute it on each click.
+//Counter logic function
 let counterclick = 0;
+const counter = () => {
+  counterclick++;
+  trycounter.innerHTML = counterclick;
+};
 
-function counter() {
-    counterclick++;
-    trycounter.innerHTML = counterclick;
-}
-
-function writeThis(word) {
-    return console.log(word.repeat(1));
-}
-
-
-//evento
-submit.addEventListener('click', guessNumber);
-
-
-
+//Click event
+submit.addEventListener("click", guessNumber);
